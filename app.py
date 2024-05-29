cc385fccee71b193fbac76af081d5a6fae03bd0e
kvnn <kriggen@gmail.com>
2024-05-29 09:32:46 -1000

    architect comment or no-comment working

--- app.py
+++ app.py
@@ -7,16 +7,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+gh_base_branch = os.environ.get('GH_BASE_BRANCH', 'main')
+gh_access_token = os.environ.get('GH_ACCESS_TOKEN', '')
+gh_repo_name = os.environ.get('GH_REPO_NAME', 'kvnn/AIAgentsStarterKit')
 architect_model_name = 'gpt-3-turbo'
 developer_model_name = 'gpt-3-turbo'
 
 gh_repo = None
 
-def get_github_info(repo_name = GH_REPO_NAME):
+def get_github_info(repo_name = gh_repo_name):
     global gh_repo
     try:
         if not gh_repo:
-            auth = Auth.Token(GH_ACCESS_TOKEN)
+            auth = Auth.Token(gh_access_token)
             gh = Github(auth=auth)
             gh_repo = gh.get_repo(repo_name)
         issues = gh_repo.get_issues(state='open')
@@ -33,9 +36,14 @@ agent_architect = Agent(
     role = "Software Architect",
     goal = """
         Take a Github Issue and create a Github Pull Request with 
-        a technical spec and implementation plan for the developer agents.
+        a technical spec and implementation plan for an automated coding agents that is powered by an LLM.
+        Since an LLM will be following this plan, be direct and clear in your instructions.
         The developer agents are good at writing code, but not at making system design decisions.
-        So, be sure to cover all fo the tracks and give them a simple implementation plan, for all of our sakes'
+        Do not mention timelines or deadlines in the spec, but do provide a clear and concise plan.
+        All code will be written in Python. 
+        Do not overcomplicate the plan, but do provide a clear and concise plan.
+        For example, if the issue calls for something as simple as "add the `reference_id` column to the get-users sql query",
+        your technical spec should be very succinct.
     """,
     backstory = """
         You are an excellent software architect. 
@@ -64,19 +72,60 @@ agent_developer = Agent(
     # llm = developer_model_name
 )
 
+architect_spec_flag = '[architect spec]'
+
+def issue_needs_architect(issue):
+    try:
+        comments = issue.get_comments()
+        
+        if not comments.totalCount:
+            return True
+        elif comments.reversed[0].body.lower().startswith('refactor'):
+            return True
+        elif comments.reversed[0].body.lower().startswith(architect_spec_flag):
+            return False
+
+        # At this point we have an ambiguous comment thread, but we know that
+        # the last comment was not a refactor or an architect spec.
+        # If there is an architect spec in the thread, we should not create a new one.
+        # Otherwise, we should.
+        for comment in comments:
+            if comment.body.lower().startswith(architect_spec_flag):
+                return False
+        return True
+    except Exception as e:
+        print(f'[issue_needs_architect] Error: {e}')
+        import pdb; pdb.set_trace()
+        return False
+
 
 def callback_architect_task(task_output, issue):
     try:
         print(f'callback_architect_task: {issue}')
-        gh_repo.create_pull(
-            base = 'kvnn',
-            head = 'main',
-            body = task_output.raw_output,
-            issue = issue
+        body = f'''{architect_spec_flag}\nc{task_output.raw_output}'''
+        comment = issue.create_comment(
+            body=body
         )
     except Exception as e:
         print(f'[callback_architect_task] Error: {e}')
         import pdb; pdb.set_trace()
+
+
+def callback_developer_task(task_output, issue):
+    base_ref = gh_repo.get_git_ref(f"heads/{gh_base_branch}")
+    new_branch_name = f"refs/heads/feature/issue-{issue.id}"
+
+    # Create a new branch from the base branch
+    gh_repo.create_git_ref(
+        ref=new_branch_name,
+        sha=base_ref.object.sha
+    )
+    gh_repo.create_pull(
+        base = gh_base_branch,
+        head = new_branch_name,
+        body = task_output.raw_output,
+        issue = issue
+    )
 
 
 def create_architect_task(issue):
@@ -105,7 +154,7 @@ def init_agents():
     for issue in issues:
         print(f'Issue: {issue}')
 
-        if not issue.pull_request:
+        if not issue.pull_request and issue_needs_architect(issue):
             architect_tasks.append(create_architect_task(issue))
     
     crew = Crew(
